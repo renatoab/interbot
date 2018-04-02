65fe060b7c1275f338fcde550563f019cff78f51
renatoab <renato_eq@yahoo.com.br>
2018-04-01 22:27:43 -0300

    Add files via upload

---
+++ InterBot.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("InterBot.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("InterBot.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("43f2a9c6-04d8-4d1e-9fb9-91915e978cdc")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ InterBot.Test/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using InterBotDomain.Contracts.Repositories;
+using InterBotDomain.DomainServices;
+using InterBotInfra.Repositories;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace InterBot.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestListCities()
+        {
+            ICitiesRepository citiesRepository = new CitiesRepository();
+
+            CitiesDomainService citiesDomainService = new CitiesDomainService(citiesRepository);
+
+            var lista = citiesDomainService.listarCities();
+
+            Assert.IsTrue(lista.Count > 0);
+        }
+    }
+}
